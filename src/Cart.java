0eaa887850eecfb13eb478be762b06dfa63b516f
ttom-eragiapreso <109075301+ttom-eragiapreso@users.noreply.github.com>
2023-03-13 15:58:18 +0100

    creato carrello basic

--- src/Cart.java
+++ src/Cart.java
@@ -1,18 +1,67 @@
 import java.math.BigDecimal;
+import java.util.Scanner;
 
 public class Cart {
     public static void main(String[] args) {
 
-        Product prodotto = new Product("Test", new BigDecimal("10.00"));
+        Scanner scan = new Scanner(System.in);
+        System.out.println("Hello! How many products would you like to add to your catalogue?");
+        int catalogueLength = Integer.parseInt(scan.nextLine());
+        Product[] catalogue = new Product[catalogueLength];
 
-        System.out.println(prodotto.getPriceWithVAT());
 
-        Smartphone iphone = new Smartphone("Iphone 10", new BigDecimal("789.99"),128);
+        for (int i = 0; i < catalogue.length; i++) {
+            System.out.println("What kind of product would you like to add?");
+            System.out.println("1 - Smartphone | 2 - Tv | 3 - Headphones");
 
-        System.out.println(iphone);
+            int productToAdd = Integer.parseInt(scan.nextLine());
 
-        Tv samsung = new Tv("SmartWow", new BigDecimal("350.99"), 45.5, true);
+            switch (productToAdd){
+                case 1 -> {
+                    System.out.println("Please insert the following details for the Smartphone:");
+                    System.out.print("Name:");
+                    String name = scan.nextLine();
+                    System.out.print("Price:");
+                    BigDecimal price = new BigDecimal(scan.nextLine());
+                    System.out.print("Storage:");
+                    int storage = Integer.parseInt(scan.nextLine());
+                    Smartphone smartphone = new Smartphone(name,price,storage);
+                    catalogue[i] = smartphone;
+                }
+                case 2 -> {
+                    System.out.println("Please insert the following details for the Tv:");
+                    System.out.print("Name:");
+                    String name = scan.nextLine();
+                    System.out.print("Price:");
+                    BigDecimal price = new BigDecimal(scan.nextLine());
+                    System.out.print("Inches:");
+                    double inches = Double.parseDouble(scan.nextLine());
+                    System.out.print("SmartTv?: 1 for YES, 2 for NO ");
+                    boolean isSmart = Integer.parseInt(scan.nextLine()) == 1;
+                    Tv tv = new Tv(name,price,inches,isSmart);
+                    catalogue[i] = tv;
+                }
+                case 3 -> {
+                    System.out.println("Please insert the following details for the Headphones:");
+                    System.out.print("Name:");
+                    String name = scan.nextLine();
+                    System.out.print("Price:");
+                    BigDecimal price = new BigDecimal(scan.nextLine());
+                    System.out.print("Colour:");
+                    String colour = scan.nextLine();
+                    System.out.print("Wireless?: 1 for YES, 2 for NO");
+                    boolean isWireless = Integer.parseInt(scan.nextLine()) == 1;
+                    Headphones headphones = new Headphones(name,price,colour,isWireless);
+                    catalogue[i] = headphones;
+                }
+                default -> {
+                    System.out.println("Please insert a valid number");
+                }
+            }
+        }
+        for (Product product : catalogue) {
+            System.out.println(product);
+        }
 
-        System.out.println(samsung);
     }
 }
---
+++ src/Headphones.java
@@ -0,0 +1,36 @@
+import java.math.BigDecimal;
+
+public class Headphones extends Product {
+    private String colour;
+    private boolean isWireless;
+
+    public Headphones(String name, BigDecimal price, String colour, boolean isWireless) {
+        super(name, price);
+        this.colour = colour;
+        this.isWireless = isWireless;
+    }
+
+    public String getColour() {
+        return colour;
+    }
+
+    public void setColour(String colour) {
+        this.colour = colour;
+    }
+
+    public boolean isWireless() {
+        return isWireless;
+    }
+
+    public void setWireless(boolean wireless) {
+        isWireless = wireless;
+    }
+
+    @Override
+    public String toString() {
+        return super.toString() + "Headphones{" +
+                "colour='" + colour + '\'' +
+                ", isWireless=" + isWireless +
+                '}';
+    }
+}
